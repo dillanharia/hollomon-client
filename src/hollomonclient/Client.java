2735728858300a57e8b7fe961a3c3a6bd6a76351
Dillan Haria <dillan.haria@gmail.com>
2026-03-25 16:06:46 +0000

    Refactored code in ConnectinoTest.java to separate its networking/parsing logic into a new file called Client.java, just to improve modularity/maintainability

---
+++ src/hollomonclient/Client.java
@@ -0,0 +1,73 @@
+package hollomonclient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class Client {
+
+	// Stores server address and port number for Hollomon connection
+    private String serverHost;
+    private int serverPort;
+
+    // Makes a client for communicating with the hollomon Server
+    public Client(String serverHost, int serverPort) {
+        this.serverHost = serverHost;
+        this.serverPort = serverPort;
+    }
+    
+    // Connects to the server, sends login details, returns any cards received
+    public ArrayList<Card> login(String username, String password) {
+        ArrayList<Card> cards = new ArrayList<>(); // List used to store card objs received from server
+
+        try (
+            Socket socket = new Socket(serverHost, serverPort);
+            PrintWriter outputWriter = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader inputReader = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()))
+        ) {
+            System.out.println("====Connected to server====");
+
+            // Sends the username/pass to server
+            outputWriter.println(username.toLowerCase());
+            outputWriter.println(password);
+
+            String serverResponse;
+
+            // Continuously read lines sent by the Hollomon server
+            while ((serverResponse = inputReader.readLine()) != null) {
+
+            	// If a card block's received, parse it and store it as a Card obj
+                if (serverResponse.equals("CARD")) {
+
+                    int id = Integer.parseInt(inputReader.readLine());
+                    String name = inputReader.readLine();
+                    Rarity rarity = Rarity.fromString(inputReader.readLine());
+                    int price = Integer.parseInt(inputReader.readLine());
+
+                    // Create a Card object from the received server data
+                    Card card = new Card(id, name, rarity, price);
+                    cards.add(card);
+
+                    System.out.println("Stored card: " + card);
+                }
+                else if (serverResponse.equals("OK")) {
+                    break;
+                }
+                else {
+                    System.out.println(serverResponse);
+                }
+            }
+
+            System.out.println("--- Connection closed ---");
+        } catch (IOException e) {
+            System.out.println("--- Could not connect to server ---");
+            e.printStackTrace();
+        }
+
+        return cards;
+    }
+}
--- src/hollomonclient/ConnectionTest.java
+++ src/hollomonclient/ConnectionTest.java
@@ -1,75 +1,18 @@
 package hollomonclient;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
 import java.util.ArrayList;
 
 public class ConnectionTest {
 
     public static void main(String[] args) {
-        connectAndLogin();
-    }
-    	
-    	
-    private static void connectAndLogin() {
-    	String serverHost = "netsrv.cim.rhul.ac.uk";
-        int serverPort = 1812;
+        Client client = new Client("netsrv.cim.rhul.ac.uk", 1812);
 
-        String username = "evidence".toLowerCase();
-        String password = "internationaltodiscover";
+        ArrayList<Card> cards = client.login("evidence", "internationaltodiscover");
 
-        try (
-            Socket socket = new Socket(serverHost, serverPort);
-            PrintWriter outputWriter = new PrintWriter(socket.getOutputStream(), true);
-            BufferedReader inputReader = new BufferedReader(
-                    new InputStreamReader(socket.getInputStream()))
-        ) {
-            System.out.println("====Connected to server====");
-
-            outputWriter.println(username);
-            outputWriter.println(password);
-
-            // List to store all cards received from hollomon server
-            ArrayList<Card> cards = new ArrayList<>();
-            
-            String serverResponse;
-            
-            // Continuously read lines sent by the hollomon server
-            while ((serverResponse = inputReader.readLine()) != null) {
-
-                if (serverResponse.equals("CARD")) {
-
-                    int id = Integer.parseInt(inputReader.readLine());
-                    String name = inputReader.readLine();
-                    Rarity rarity = Rarity.fromString(inputReader.readLine());
-                    int price = Integer.parseInt(inputReader.readLine());
-
-                    // Create a Card object from the received server data
-                    Card card = new Card(id, name, rarity, price);
-                    cards.add(card);
-                   
-                    System.out.println("Stored card: " + card);
-                }
-                else if (serverResponse.equals("Server has finished sending data")) {
-                    break;
-                }
-                else {
-                    System.out.println(serverResponse);
-                }
-            }
-            // Display all stored cards after receiving
-            System.out.println("\n--- All Cards Have been received ---");
-            for (Card c : cards) {
-                System.out.println(c);
-            }
-
-            System.out.println("--- Connection closed ---");
-        } catch (IOException e) {
-            System.out.println("--- Could not connect to server ---");
-            e.printStackTrace();
+        
+        System.out.println("\n--- All Cards Have Been Received ---");
+        for (Card card : cards) {
+            System.out.println(card);
         }
     }
 }
